53b6f824bd8c5f192a1d808b51a12c80a1dd44ee
Prince Khatri <princekhatri1013@gmail.com>
2026-04-21 16:37:57 +0530

    Add logging configuration and initialize components

--- src/logger.py
+++ src/logger.py
@@ -16,3 +16,10 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(
+    logging.Formatter("[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s")
+)
+logging.getLogger().addHandler(console_handler)
+
